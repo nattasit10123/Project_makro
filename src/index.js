3f6fc42be2ac171f15ff3803772667c31c8b01f6
nattasit10123 <nattasit5054@gmail.com>
2024-01-23 23:51:31 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./middlewares/error");
+const notFoundHandler = require("./middlewares/notFound");
+const createError = require("./utils/createError");
+
+const authRoute = require("./routes/auth-route");
+const productRoute = require("./routes/product-route");
 
 const app = express();
 
-const port = process.env.PORT
-app.listen(port , ()=>{
-    console.log("Server run on port"+""+port);
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoute);
+app.use("/product", productRoute);
+
+app.use(errorHandler);
+app.use("*", notFoundHandler);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server run on port" + " " + port);
 });
